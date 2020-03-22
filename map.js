3c229bccf51a1547ee1c455732857e47a84b76fd
John Murray <j2murray12@gmail.com>
2020-03-22 17:33:34 -0400

    Update map.js

--- map.js
+++ map.js
@@ -26,8 +26,8 @@ Promise.all([d3.json(centralParkJSON),d3.csv(squirrelDataURL)])
         })
 		console.log(squirrelDataLong[217])
 
-        const width = 1200;	
-        const height = 700;
+        const width = 1000;	
+        const height = 500;
         
 		let svg = d3.select("div.map-container")
             .append("svg")
