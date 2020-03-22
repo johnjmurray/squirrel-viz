de4fc10f0bf3c2394f51495f9354d8bc1ecdb356
John Murray <j2murray12@gmail.com>
2020-03-22 17:28:04 -0400

    Update map.js

--- map.js
+++ map.js
@@ -2,16 +2,14 @@
 
 //const squirrelDataURL = `https://data.cityofnewyork.us/resource/vfnx-vebw.geojson`;
 const squirrelDataURL = `https://data.cityofnewyork.us/resource/vfnx-vebw.csv`;
-//const centralParkJSON = 'centralPark.json';
+const centralParkJSON = 'centralPark.json';
 
 
 
 Promise.all([d3.json(centralParkJSON),d3.csv(squirrelDataURL)])
-//Promise.all([d3.csv(squirrelDataURL)])
     .then(result => {
         let centralPark = result[0],squirrelData = result[1]
-		//let squirrelData = result[0]
-        // Reshape from wide to long format.
+		// Reshape from wide to long format.
 		let squirrelDataLong = [];
         let goodCols = ["PrimaryFurColor", "SquirrelID", "Lat", "Long"]
         squirrelData.forEach(row => {
@@ -37,9 +35,9 @@ Promise.all([d3.json(centralParkJSON),d3.csv(squirrelDataURL)])
             .attr("viewBox", `0 0 ${width} ${height}`)
             .classed("svg-content", true);
 		//https://stackoverflow.com/questions/28116230/displaying-ny-state-with-counties-map-via-shp-and-topojson
-        let projection = d3.geoMercator().center([-74,41]).scale(100).translate([width, height]);	
+        let projection = d3.geoMercator().center([-73.9859388,40.7712531]).scale(1000000).translate([width/2, height/2]);	
         let path = d3.geoPath().projection(projection);
-
+		let uniqueSquirrels = d3.map(squirrelDataLong, d => d.SquirrelID).keys()
         // Bind the data to the SVG and create one path per GeoJSON feature
         svg.append("g")
             .selectAll("path")
@@ -49,16 +47,15 @@ Promise.all([d3.json(centralParkJSON),d3.csv(squirrelDataURL)])
             .attr("d", path)
 
 
-
-        svg
-            .selectAll("caseCircles")
-            .data(squirrelDataLong)
-            .enter()
-            .append("circle")
-            .attr("class", "circle")
-            .attr("cx", d => projection([d.Long, d.Lat])[40])
-            .attr("cx", d => projection([d.Long, d.Lat])[40])
-			.attr("r","100")
+		svg
+			.selectAll("caseCircles")
+			.data(squirrelDataLong)
+			.enter()
+			.append("circle")
+			.attr("class", "circle")
+			.attr("cx", d => projection([d.Long, d.Lat])[0])
+			.attr("cy", d => projection([d.Long, d.Lat])[1])
+			.attr("r","1")
 
     })
     .catch(err => console.error(err));
